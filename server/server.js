82177563f3fdd328680bdadd1b1b37e93af12323
JayCogen44 <jaycog44@gmail.com>
2019-02-11 14:58:25 -0800

    updated server greeting

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3477;
 
 app.get('/', (req, res) => {
-    res.send('Welcome back Aetos');
+    res.send('Welcome back Aetos this is working');
 });
 
 app.listen(PORT, () => console.log(`AETOS node server is listening on PORT: ${PORT}`));
