4138f48d7167d2ce9769c2154c097c794860354e
trischoe <tristans7@gmail.com>
2019-02-14 15:12:53 -0800

    fixed time not being updated

--- server/queryController.js
+++ server/queryController.js
@@ -7,15 +7,15 @@ const reqHeader = {
         Accept : 'application/json',
         Authorization : apiKey
 };
-// start is six hours ago
-let startTime = Math.floor((Date.now()/1000 - (3600*6)));
-let endTime = Math.floor((Date.now()/1000));
-let step = 30;
+
 queryController = {};
 
 
 
 queryController.getCpu = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(rate(container_cpu_usage_seconds_total%7Bnamespace%3D%22default%22%2Cpod_name%3D~%22frontend.*%22%7D%5B3m%5D))'
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -29,6 +29,9 @@ queryController.getCpu = (req, res, next) => {
 }
 
 queryController.getNetworkTraffic = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(rate(container_network_transmit_bytes_total{namespace="default",pod_name=~"frontend.*"}[3m]))%20%2B%20sum(rate(container_network_receive_bytes_total{namespace="default",pod_name=~"frontend.*"}[3m]))';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -42,6 +45,9 @@ queryController.getNetworkTraffic = (req, res, next) => {
 }
 
 queryController.getMemUsage = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(container_memory_usage_bytes%7Bnamespace%3D%22default%22%2Cpod_name%3D~%22frontend.*%22%7D)%20%2F%201024%5E3';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -55,6 +61,9 @@ queryController.getMemUsage = (req, res, next) => {
 }
 
 queryController.getNodeCount = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = ':kube_pod_info_node_count:';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -68,6 +77,9 @@ queryController.getNodeCount = (req, res, next) => {
 }
 
 queryController.getCpuUtilization = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(rate(node_cpu{mode!="idle",mode!="iowait",mode!~"^(?:guest.*)$"}[5m])) BY (instance)';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -81,6 +93,9 @@ queryController.getCpuUtilization = (req, res, next) => {
 }
 
 queryController.getSaturation = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(node_load1) by (node) / count(node_cpu{mode="system"}) by (node) * 100';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -94,6 +109,9 @@ queryController.getSaturation = (req, res, next) => {
 }
 
 queryController.getMemoryUtilization = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = '1 - sum(node_memory_MemAvailable) by (node) / sum(node_memory_MemTotal) by (node)';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
@@ -107,6 +125,9 @@ queryController.getMemoryUtilization = (req, res, next) => {
 }
 
 queryController.getNetworkSaturation = (req, res, next) => {
+    let startTime = Math.floor((Date.now()/1000 - (3600*6)));
+    let endTime = Math.floor((Date.now()/1000));
+    let step = 30;
     let queryStr = 'sum(rate(node_network_receive_bytes[5m])) by (node) %20%2B%20 sum(rate(node_network_transmit_bytes[5m])) by (node)';
     let urlVal = `${grafurl}query=${queryStr}&start=${startTime}&end=${endTime}&step=${step}`;
     request({headers : reqHeader,
