cfffcb055f837cf4955b64fd235991b22cbf25f5
JayCogen44 <jaycog44@gmail.com>
2019-02-16 14:11:15 -0800

    fixed merge conflict

--- client/components/saturationChart.js
+++ client/components/saturationChart.js
@@ -29,7 +29,6 @@ export default class SaturationChart extends Component {
         return (
             <View>
                 <View style={styles.headerWrapper}>
-<<<<<<< HEAD
                     <Text style={styles.header}>
                         Saturation
                         <Button
@@ -38,9 +37,6 @@ export default class SaturationChart extends Component {
                         // onPress={this.props.updateTime}
                         />
                     </Text>
-=======
-                    <Text style={styles.header}>Saturation</Text>
->>>>>>> master
                 </View>
                 <VictoryChart
                     responsive={true}
